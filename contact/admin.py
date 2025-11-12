f8e1da383db5c2ad4c35277624418935e11eb2cc
Jaderson Silva <jgs_op@hotmail.com>
2025-11-12 17:19:32 -0300

    Configurated the archive in table  app in Django

--- contact/admin.py
+++ contact/admin.py
@@ -4,12 +4,13 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'phone', 'email', 'created_date')
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'email', 'show', 'created_date')
     ordering = ('-id',)
     search_fields = ('id', 'first_name', 'last_name', 'email', 'phone')
     list_filter = ('created_date',)
     list_per_page = 10
     list_max_show_all = 100
+    list_editable = ('show',)
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
